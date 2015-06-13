b6b543023414584cdab79456817d0c0f79823507
Subhransu Behera <subh@subhb.org>
2015-06-13 16:20:27 -0700

    Added back Asset Catalog

--- SmileApp Extension/NotificationController.swift
+++ SmileApp Extension/NotificationController.swift
@@ -29,7 +29,7 @@ class NotificationController: WKUserNotificationInterfaceController {
         super.didDeactivate()
     }
 
-    /*
+  
     override func didReceiveLocalNotification(localNotification: UILocalNotification, withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
         // This method is called when a local notification needs to be presented.
         // Implement it if you use a dynamic notification interface.
@@ -38,16 +38,17 @@ class NotificationController: WKUserNotificationInterfaceController {
         // After populating your dynamic notification interface call the completion block.
         completionHandler(.Custom)
     }
-    */
-    
-    /*
+
     override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
         // This method is called when a remote notification needs to be presented.
         // Implement it if you use a dynamic notification interface.
         // Populate your dynamic notification interface as quickly as possible.
         //
+      
+        print("lala")
+        
         // After populating your dynamic notification interface call the completion block.
         completionHandler(.Custom)
     }
-    */
+    
 }
