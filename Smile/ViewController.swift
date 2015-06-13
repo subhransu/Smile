eca127749021f1b1d883a46888b996e25d518f16
Subhransu Behera <subh@subhb.org>
2015-06-13 11:08:35 -0700

    Added the blank project

---
+++ Smile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smile
+//
+//  Created by Behera, Subhransu on 6/13/15.
+//  Copyright © 2015 subhb.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ SmileApp Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SmileApp Extension
+//
+//  Created by Behera, Subhransu on 6/13/15.
+//  Copyright © 2015 subhb.org. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
